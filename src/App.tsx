57a307da98dc5cdb9bf1b72af59da72fe531fd48
Matheus Silva <teus_26@outlook.com>
2024-09-19 16:03:41 -0300

    git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Form } from './components/Form'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">React Hook Form com Typescript</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
