4b821994d395c8678dda234791e608d908076797
Matheus Silva <teus_26@outlook.com>
2024-09-20 11:25:17 -0300

    validando select e email

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -21,6 +21,46 @@ export function Form() {
         />
         {errors.name && <small className="text-red-500 italic">{errors.name.message}</small>}
       </div>
+
+      <div className="flex flex-col gap-2 mb-2">
+        <label htmlFor="lastname">Sobrenome</label>
+        <input 
+          type="text" 
+          id="lastname"
+          placeholder="Digite o seu sobrenome"
+          {...register("lastname")}
+          className="h-10 pl-2 rounded-md border border-slate-300"
+        />
+        {errors.lastname && <small className="text-red-500 italic">{errors.lastname.message}</small>}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-2">
+        <label htmlFor="gender">Gênero</label>
+        <select 
+          id="gender" 
+          {...register("gender")}
+          className="h-10 pl-1 rounded-md border border-slate-300"
+        >
+          <option value="select">Selecione</option>
+          <option value="male">Masculino</option>
+          <option value="female">Feminino</option>
+          <option value="other">Outro</option>
+        </select>
+        {errors.gender && <small className="text-red-500 italic">{errors.gender.message}</small>}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-2">
+        <label htmlFor="email">E-mail</label>
+        <input 
+          type="email" 
+          id="email"
+          placeholder="Digite o seu e-mail"
+          {...register("email")}
+          className="h-10 pl-2 rounded-md border border-slate-300"
+        />
+        {errors.email && <small className="text-red-500 italic">{errors.email.message}</small>}
+      </div>
+
       <button type="submit" className="w-full h-11 bg-cyan-400 rounded-md my-2 hover:bg-cyan-500 transition-all">Cadastrar</button>
     </form>
   );
